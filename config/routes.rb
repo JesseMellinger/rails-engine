02677027bb14e1df8509fb73bfbe665d19b65c54
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-12 19:13:21 -0700

    Update routes for merchant and item controllers with shorter syntax

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show, :create, :update]
-      resources :merchants, only: [:index, :show, :create, :update]
+      resources :items, except: [:new, :edit]
+      resources :merchants, except: [:new, :edit]
     end
   end
 end
