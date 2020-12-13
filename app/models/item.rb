c73bb03c2f508f1acec2972b513fe80b847cdaa5
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-12 19:14:12 -0700

    Create private class method to run before destroying item to check for invoice deletion

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
+  before_destroy :destroy_invoices
+
   belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+
+  private
+  def destroy_invoices
+    self.invoices.each do |invoice|
+      invoice_item = InvoiceItem.find_record(self.id, invoice.id)
+      InvoiceItem.delete(invoice_item.id)
+      Invoice.delete(invoice.id) if invoice.can_be_deleted?
+    end
+  end
 end
