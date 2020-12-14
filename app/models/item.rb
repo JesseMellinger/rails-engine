e8ee96230abab31602df881005872d9ac21d2759
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-14 14:15:39 -0700

    Create class methods to find all records given criteria in query parameters

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,22 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
+  def self.find_by_attribute(attribute, value)
+    if attribute == "created_at" || attribute == "updated_at"
+      where("Date(#{attribute}) = ?", "#{value}").first
+    else
+      where("lower(#{attribute}) like ?", "%#{value.downcase}%").first
+    end
+  end
+
+  def self.find_all_by_attribute(attribute, value)
+    if attribute == "created_at" || attribute == "updated_at"
+      where("Date(#{attribute}) = ?", "#{value}")
+    else
+      where("lower(#{attribute}) like ?", "%#{value.downcase}%")
+    end
+  end
+
   private
   def destroy_invoices
     self.invoices.each do |invoice|
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,19 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
+
+  def self.find_by_attribute(attribute, value)
+    if attribute == "created_at" || attribute == "updated_at"
+      where("Date(#{attribute}) = ?", "#{value}").first
+    else
+      where("lower(#{attribute}) like ?", "%#{value.downcase}%").first
+    end
+  end
+
+  def self.find_all_by_attribute(attribute, value)
+    if attribute == "created_at" || attribute == "updated_at"
+      where("Date(#{attribute}) = ?", "#{value}")
+    else
+      where("lower(#{attribute}) like ?", "%#{value.downcase}%")
+    end
+  end
 end
