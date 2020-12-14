2fc3bf6052e8cc2f723aa644dc53741b23bd94fc
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-14 14:16:09 -0700

    Create actions to find single record and collection of records

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -20,9 +20,22 @@ class Api::V1::MerchantsController < ApplicationController
     render body: nil, status: 204
   end
 
+  def find
+    key = merchant_params.keys.first
+    value = merchant_params.values.first.to_s
+    merchant = Merchant.find_by_attribute(key, value)
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  def find_all
+    key = merchant_params.keys.first
+    value = merchant_params.values.first.to_s
+    merchants = Merchant.find_all_by_attribute(key, value)
+    render json: MerchantSerializer.new(merchants)
+  end
+
   private
   def merchant_params
-    return params.require(:merchant).permit(:name) if params[:merchant]
-    params.permit(:name)
+    params.permit(:name, :created_at, :updated_at)
   end
 end
