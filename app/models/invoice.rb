24bd7963d352cf71a5626c4d7dd26df26a5a6872
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-15 16:27:57 -0700

    Add relationships that invoice has many transactions and merchants has many invoice items through invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,6 +3,7 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :items, through: :invoice_items
+  has_many :transactions
 
   def can_be_deleted?
     invoice_items.empty?
