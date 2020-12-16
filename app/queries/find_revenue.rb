6fef561dc87d13a5b57ed1faf694a3d06b6452d4
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-15 18:46:51 -0700

    Create new class method to find total revenue of merchant

--- app/queries/find_revenue.rb
+++ app/queries/find_revenue.rb
@@ -1,12 +1,13 @@
 class FindRevenue
-  def initialize(start_date, end_date)
-    @start_date = start_date
-    @end_date = end_date
+  def self.find_revenue_by_dates(start_date, end_date)
+    connection = ActiveRecord::Base.connection
+    sql = "SELECT sum(invoice_items.quantity * invoice_items.unit_price) as revenue FROM merchants INNER JOIN items ON items.merchant_id = merchants.id INNER JOIN invoice_items ON invoice_items.item_id = items.id INNER JOIN invoices ON invoices.id = invoice_items.invoice_id INNER JOIN transactions ON transactions.invoice_id = invoices.id WHERE (transactions.result = 'success' and invoices.status = 'shipped' and Date(invoices.created_at) >= '#{start_date}' and Date(invoices.created_at) <= '#{end_date}')"
+    connection.select_values(sql)
   end
 
-  def find_revenue_by_dates()
+  def self.find_revenue_of_merchant(merchant_id)
     connection = ActiveRecord::Base.connection
-    sql = "SELECT sum(invoice_items.quantity * invoice_items.unit_price) as revenue FROM merchants INNER JOIN items ON items.merchant_id = merchants.id INNER JOIN invoice_items ON invoice_items.item_id = items.id INNER JOIN invoices ON invoices.id = invoice_items.invoice_id INNER JOIN transactions ON transactions.invoice_id = invoices.id WHERE (transactions.result = 'success' and invoices.status = 'shipped' and Date(invoices.created_at) >= '#{@start_date}' and Date(invoices.created_at) <= '#{@end_date}')"
+    sql = "SELECT sum(invoice_items.quantity * invoice_items.unit_price) as total_revenue FROM merchants INNER JOIN items ON items.merchant_id = merchants.id INNER JOIN invoice_items ON invoice_items.item_id = items.id INNER JOIN invoices ON invoices.id = invoice_items.invoice_id INNER JOIN transactions ON transactions.invoice_id = invoices.id WHERE (transactions.result = 'success' and invoices.status = 'shipped' and merchants.id = '#{merchant_id}')"
     connection.select_values(sql)
   end
 end
